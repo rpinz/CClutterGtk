d374bebf6e1c53b358a1516f80c84a94cc3dd737
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-27 15:33:52 +0200

    Add structs so typedefs get exported

--- clutter_gtk_bridging.h
+++ clutter_gtk_bridging.h
@@ -1,2 +1,14 @@
+struct _GtkClutterActor {};
+struct _GtkClutterActorClass {};
+struct _GtkClutterActorPrivate {};
+struct _GtkClutterEmbed {};
+struct _GtkClutterEmbedClass {};
+struct _GtkClutterEmbedPrivate {};
+struct _GtkClutterTexture {};
+struct _GtkClutterTextureClass {};
+struct _GtkClutterWindow {};
+struct _GtkClutterWindowClass {};
+struct _GtkClutterWindowPrivate {};
+
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
 #include <clutter-gtk/clutter-gtk.h>
