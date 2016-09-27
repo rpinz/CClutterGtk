aefcff7021ff9222f01f9ef9f5fdd8765a341709
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-27 15:58:26 +0200

    Remove redefined structs

--- clutter_gtk_bridging.h
+++ clutter_gtk_bridging.h
@@ -1,13 +1,5 @@
-struct _GtkClutterActor {};
-struct _GtkClutterActorClass {};
 struct _GtkClutterActorPrivate {};
-struct _GtkClutterEmbed {};
-struct _GtkClutterEmbedClass {};
 struct _GtkClutterEmbedPrivate {};
-struct _GtkClutterTexture {};
-struct _GtkClutterTextureClass {};
-struct _GtkClutterWindow {};
-struct _GtkClutterWindowClass {};
 struct _GtkClutterWindowPrivate {};
 
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
